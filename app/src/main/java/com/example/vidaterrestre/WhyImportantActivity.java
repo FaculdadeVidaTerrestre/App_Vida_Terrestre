68f01d7197279cbbfc0b292652fb245e113018ee
Monica <bytefixer@hotmail.com>
2024-11-01 11:17:25 -0300

    vida_terrestre_add_whyimportant

--- app/src/main/java/com/example/vidaterrestre/WhyImportantActivity.java
+++ app/src/main/java/com/example/vidaterrestre/WhyImportantActivity.java
@@ -1,6 +1,14 @@
 package com.example.vidaterrestre;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class WhyImportantActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.whyimportant_activity);
+
+    }
 }
